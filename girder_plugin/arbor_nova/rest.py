76e66bd2c2a7896bd96faec92b790ba3e79b33b5
Michael Grauer <michael.grauer@kitware.com>
2019-04-08 20:42:19 +0000

    Use GW transforms more idiomatically

--- girder_plugin/arbor_nova/rest.py
+++ girder_plugin/arbor_nova/rest.py
@@ -6,8 +6,7 @@ from arbor_nova_tasks import column_append
 from girder.api import access
 from girder.api.describe import Description, autoDescribeRoute
 from girder.api.rest import filtermodel, Resource
-from girder.models.token import Token
-from girder_worker_utils.transforms import girder_io
+from girder_worker_utils.transforms.girder_io import GirderFileId, GirderUploadToItem
 
 
 class ArborNova(Resource):
@@ -27,10 +26,7 @@ class ArborNova(Resource):
         .errorResponse('Failed to upload output file.', 500)
     )
     def csv_column_append(self, fileId, itemId):
-        token = Token().createToken(user=self.getCurrentUser())
-        token_id = str(token['_id'])
-        result = column_append.delay(in_filepath=girder_io.GirderFileId(fileId),
-                                     output_item_id=itemId,
-                                     girder_client_token=token_id)
+        result = column_append.delay(GirderFileId(fileId),
+                                     girder_result_hooks=[GirderUploadToItem(itemId)])
 
         return result.job
--- girder_worker_tasks/arbor_nova_tasks/__init__.py
+++ girder_worker_tasks/arbor_nova_tasks/__init__.py
@@ -1,10 +1,6 @@
-import os
-import shutil
-import tempfile
 from girder_worker import GirderWorkerPluginABC
 from girder_worker.app import app
 from girder_worker.utils import girder_job
-from girder_worker_utils.transforms import girder_io
 
 
 class ArborNovaTasksGirderWorkerPlugin(GirderWorkerPluginABC):
@@ -19,24 +15,12 @@ class ArborNovaTasksGirderWorkerPlugin(GirderWorkerPluginABC):
 
 @girder_job(title='ColumnAppendTask')
 @app.task(bind=True)
-def column_append(self, in_filepath, output_item_id, **kwargs):
-    tmpdir = tempfile.mkdtemp()
+def column_append(self, in_filepath, **kwargs):
     outname = 'outfile.csv'
+    with open(outname, 'w') as tmp:
+        with open(in_filepath, 'r') as csv:
+            for line in csv:
+                outline = line.strip() + ', newcol\n'
+                tmp.write(outline)
 
-    # Ensure the file is read/write by the creator only
-    saved_umask = os.umask(0o077)
-
-    outpath = os.path.join(tmpdir, outname)
-    try:
-        with open(outpath, 'w') as tmp:
-            with open(in_filepath, 'r') as csv:
-                for line in csv:
-                    outline = line.strip() + ', newcol\n'
-                    tmp.write(outline)
-        guti = girder_io.GirderUploadToItem(output_item_id, gc=self.girder_client)
-        guti.transform(tmp.name)
-    finally:
-        os.umask(saved_umask)
-        shutil.rmtree(tmpdir, ignore_errors=True)
-
-    return 'celery got string %s' % in_filepath
+    return outname

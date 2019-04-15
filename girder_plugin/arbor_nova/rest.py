82cc01d2ac2fde87865d051e75a9546462eeb5cc
dorukozturk <doruk.ozturk@kitware.com>
2019-04-15 17:58:56 -0400

    Add an endpoint which authenticates as the anonymous user

--- girder_plugin/arbor_nova/rest.py
+++ girder_plugin/arbor_nova/rest.py
@@ -5,15 +5,18 @@
 from arbor_nova_tasks import column_append
 from girder.api import access
 from girder.api.describe import Description, autoDescribeRoute
-from girder.api.rest import filtermodel, Resource
+from girder.api.rest import filtermodel, Resource, setCurrentUser
+from girder.models.user import User
 from girder_worker_utils.transforms.girder_io import GirderFileId, GirderUploadToItem
 
 
 class ArborNova(Resource):
-    def __init__(self):
+    def __init__(self, anonymous_user):
         super(ArborNova, self).__init__()
         self.resourceName = 'arbor_nova'
         self.route('POST', ('csvColumnAppend', ), self.csv_column_append)
+        self.route('POST', ('anonlogin',), self.anonymousLogin)
+        self.anonymous_user = anonymous_user
 
     @access.token
     @filtermodel(model='job', plugin='jobs')
@@ -30,3 +33,27 @@ class ArborNova(Resource):
                                      girder_result_hooks=[GirderUploadToItem(itemId)])
 
         return result.job
+
+    @autoDescribeRoute(
+        Description('Log in using the "anonymous user".')
+    )
+    @access.public
+    def anonymousLogin(self, params):
+        """Log in using the "anonymous user"."""
+        user = User().findOne({
+            'login': self.anonymous_user
+        })
+        user = User().filter(user, user)
+
+        setCurrentUser(user)
+        token = self.sendAuthTokenCookie(user)
+
+        return {
+            'user': user,
+            'authToken': {
+                'token': token['_id'],
+                'expires': token['expires'],
+                'scope': token['scope']
+            },
+            'message': 'Anonymous login succeeded.'
+        }

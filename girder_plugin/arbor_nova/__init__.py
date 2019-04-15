7e36d099f08abc998dbaffe4d95991118b028ab6
dorukozturk <doruk.ozturk@kitware.com>
2019-04-15 16:35:57 -0400

    Add an anonymous user for running tasks without auth

--- girder_plugin/arbor_nova/__init__.py
+++ girder_plugin/arbor_nova/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from girder.plugin import getPlugin, GirderPlugin
+from girder.models.user import User
 from . import rest
 
 
@@ -9,6 +10,25 @@ class ArborNovaGirderPlugin(GirderPlugin):
     DISPLAY_NAME = 'Arbor Nova'
 
     def load(self, info):
+        ANONYMOUS_USER = 'anonymous'
+
+        anon_user = User().findOne({
+            'login': ANONYMOUS_USER
+        })
+
+        if not anon_user:
+            anon_user = User().createUser(
+                login=ANONYMOUS_USER,
+                password=None,
+                firstName='Public',
+                lastName='User',
+                email='anon@example.com',
+                admin=False,
+                public=False)
+            anon_user['status'] = 'enabled'
+
+            anon_user = User().save(anon_user)
+
         getPlugin('jobs').load(info)
 
         info['apiRoot'].arbor_nova = rest.ArborNova()

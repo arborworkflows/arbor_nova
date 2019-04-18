a1f6d4f02bb896f8fdc04137ec30885f045ec3ce
Doruk Ozturk <doruk.ozturk@kitware.com>
2019-04-18 14:52:35 -0400

    Revert the endpoint so that it has a destination item

--- girder_plugin/arbor_nova/rest.py
+++ girder_plugin/arbor_nova/rest.py
@@ -6,8 +6,6 @@ from arbor_nova_tasks import column_append
 from girder.api import access
 from girder.api.describe import Description, autoDescribeRoute
 from girder.api.rest import filtermodel, Resource
-from girder.models.folder import Folder
-from girder.models.item import Item
 from girder_worker_utils.transforms.girder_io import GirderFileId, GirderUploadToItem
 
 
@@ -22,18 +20,12 @@ class ArborNova(Resource):
     @autoDescribeRoute(
         Description('Append a new column to a csv file.')
         .param('fileId', 'The ID of the input file.')
+        .param('itemId', 'The ID of the output item where the output file will be uploaded.')
         .errorResponse()
         .errorResponse('Write access was denied on the parent item.', 403)
         .errorResponse('Failed to upload output file.', 500)
     )
-    def csv_column_append(self, fileId):
-        result_folder = Folder().findOne({
-            'name': 'Results'
-        })
-        item = Item().createItem('{}-{}'.format(self.resourceName, fileId),
-                                 creator=self.getCurrentUser(),
-                                 folder=result_folder)
-        itemId = str(item['_id'])
+    def csv_column_append(self, fileId, itemId):
         result = column_append.delay(GirderFileId(fileId),
                                      girder_result_hooks=[GirderUploadToItem(itemId)])
 

ce0c89fbf58c7094e0e10ed70927656b86f5fed0
Jeffrey Baumes <jeff.baumes@kitware.com>
2019-04-23 13:45:47 -0400

    Finishing PGLS Vuetify

--- client/src/apps/PGLS.vue
+++ client/src/apps/PGLS.vue
@@ -1,58 +1,98 @@
 <template>
   <v-app>
-		<v-content>
-			<v-container fluid>
-				<v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
-          <v-text-field label="Select Table" @click='selectTableFile' v-model='tableFileName'></v-text-field>
-          <input
-            type="file"
-            style="display: none"
-            ref="tableFile"
-            @change="uploadTable($event.target.files[0])"
-          >
-				</v-flex>
-				<v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
-          <v-text-field label="Select Tree" @click='selectTreeFile' v-model='treeFileName'></v-text-field>
-          <input
-            type="file"
-            style="display: none"
-            ref="treeFile"
-            @change="uploadTree($event.target.files[0])"
-          >
-				</v-flex>
-    <div>
-      <v-radio-group v-model="correlation" row>
-        <template v-slot:label>
-          <div>Correlation</div>
+    <v-layout class="transform-view" row fill-height>
+      <v-navigation-drawer permanent fixed style="width: 400px; min-width: 400px;">
+        <v-toolbar dark flat color="primary">
+          <v-toolbar-title class="white--text">PGLS</v-toolbar-title>
+        </v-toolbar>
+        <v-container fluid>
+          <v-flex xs12>
+            <v-btn class="text-none" outline block @click='$refs.tableFile.click()'>{{ tableFileName || 'UPLOAD TABLE' }}</v-btn>
+            <input
+              type="file"
+              style="display: none"
+              ref="tableFile"
+              @change="uploadTable($event.target.files[0])"
+            >
+          </v-flex>
+          <v-flex xs12>
+            <v-btn class="text-none" outline block @click='$refs.treeFile.click()'>{{ treeFileName || 'UPLOAD TREE' }}</v-btn>
+            <input
+              type="file"
+              style="display: none"
+              ref="treeFile"
+              @change="uploadTree($event.target.files[0])"
+            >
+          </v-flex>
+          <v-flex xs12>
+            <v-select label="Correlation" v-model="correlation" :items="correlationOptions" />
+          </v-flex>
+          <v-flex xs12>
+            <v-select label="Independent Variable" v-model="independentVariable" :items="columns" />
+          </v-flex>
+          <v-flex xs12>
+            <v-select label="Dependent Variable" v-model="dependentVariable" :items="columns" />
+          </v-flex>
+          <v-flex xs12>
+            <v-btn
+              block
+              :class="{ primary: readyToRun }"
+              :flat="readyToRun"
+              :outline="!readyToRun"
+              :disabled="!readyToRun"
+              @click="run"
+            >
+              Go
+            </v-btn>
+          </v-flex>
+        </v-container>
+      </v-navigation-drawer>
+      <v-layout column justify-start fill-height style="margin-left: 400px">
+          <v-card class="ma-4">
+            <v-card-text>
+              <b>PGLS (Phylogenetic generalized least squares)</b> is a method for testing for correlations of
+              two continuously distributed characters that vary across species. For example, one might be interested in
+              the correlation between body size (X) and metabolic rate (Y).
+              <br><br>
+              The phylogenetic tree is used to account for the fact that species are not independent data points. In this
+              case, the tree is used to <b>explain correlations in the residuals</b> from the regression line of Y against X.
+              <br><br>
+              In this app, you can model the residuals under three models: Brownian motion (BM), a random-walk model common
+              for comparative methods; Ornstein-Uhlenbeck (OU), a model where divergence is constrained away from some optimal value,
+              in this case the regression line itself; and Pagel's lambda, a model that rescales the tree to effectively
+              change the rate of divergence through time.
+              <br><br>
+              When you carry out this analysis, you are assuming that both the species values and the tree are <b>known without error</b>,
+              that the relationship between X and Y can be described with a <b>straight line</b>, and that the <b>model you select is reasonable</b>
+              as a description of how residuals from that line change through time.
+              <br><br>
+              1. Upload your table (.csv) and tree (Newick, .phy).
+              <br><br>
+              2. Select the desired correlation mode, and independent and dependent variables.
+              <br><br>
+              3. Click GO.
+            </v-card-text>
+          </v-card>
+          <v-card v-if="table.length > 0" class="mb-4 ml-4 mr-4">
+            <v-card-text>{{ tableFileName }}</v-card-text>
+            <json-data-table :data="table" />
+          </v-card>
+        <div v-if="running" xs12 class="text-xs-center mb-4 ml-4 mr-4">
+          Running (Job Status {{ job.status }}) ...
+        </div>
+        <code v-if="!running && job.status === 4" class="mb-4 ml-4 mr-4" style="width: 100%">{{ job.log.join('\n') }}</code>
+        <template v-if="!running && job.status === 3">
+          <v-card v-if="result.length > 0" class="mb-4 ml-4 mr-4">
+            <v-card-text>Model fit summary</v-card-text>
+            <json-data-table :data="result" hide-actions/>
+          </v-card>
+          <v-card class="mb-4 ml-4 mr-4">
+            <v-card-text>Correlation plot</v-card-text>
+            <img :src="plotUrl" style="display: block; margin: auto">
+          </v-card>
         </template>
-        <v-radio label="BM" value="BM"></v-radio>
-        <v-radio label="OU" value="OU"></v-radio>
-        <v-radio label="lambda" value="lambda"></v-radio>
-      </v-radio-group>
-    </div>
-    <div>
-      <v-radio-group v-model="independentVariable" row>
-        <template v-slot:label>
-          <div>Independent Variable</div>
-        </template>
-        <v-radio v-for="col in columns" v-bind:key="col" :label="col" :value="col"></v-radio>
-      </v-radio-group>
-    </div>
-    <div>
-      <v-radio-group v-model="dependentVariable" row>
-        <template v-slot:label>
-          <div>Dependent Variable</div>
-        </template>
-        <v-radio v-for="col in columns" v-bind:key="col" :label="col" :value="col"></v-radio>
-      </v-radio-group>
-    </div>
-    <div>
-      <v-btn @click="run">Run</v-btn>
-    </div>
-    <div>Job status: {{ job.status }}</div>
-    <div>{{ result }}</div>
-    <img :src="plotUrl">			</v-container>
-		</v-content>
+      </v-layout>
+    </v-layout>
   </v-app>
 </template>
 
@@ -62,36 +102,55 @@ import { csvParse } from 'd3-dsv';
 import scratchFolder from '../scratchFolder';
 import pollUntilJobComplete from '../pollUntilJobComplete';
 import optionsToParameters from '../optionsToParameters';
+import JsonDataTable from '../components/JsonDataTable';
 
 export default {
   name: 'pgls',
   inject: ['girderRest'],
+  components: {
+    JsonDataTable,
+  },
   data: () => ({
     tableFile: {},
+    table: [],
     treeFile: {},
     tableFileName: '',
     treeFileName: '',
     job: { status: 0 },
-    result: '',
-    plotUrl: '',
+    running: false,
     columns: [],
     correlation: 'BM',
+    correlationOptions: ['BM', 'OU', 'lambda'],
     independentVariable: null,
     dependentVariable: null,
+    result: [],
+    resultColumns: [],
+    plotUrl: '',
   }),
   asyncComputed: {
     scratchFolder() {
       return scratchFolder(this.girderRest);
     },
   },
+  computed: {
+    readyToRun() {
+      return !!this.treeFileName &&
+        !!this.tableFileName &&
+        !!this.independentVariable &&
+        !!this.dependentVariable;
+    },
+  },
   methods: {
     async run() {
+      this.running = true;
+      this.errorLog = null;
       const modelFitSummaryItem = (await this.girderRest.post(
         `item?folderId=${this.scratchFolder._id}&name=result`,
-      )).data;
+      )).data
       const plotItem = (await this.girderRest.post(
         `item?folderId=${this.scratchFolder._id}&name=result`,
-      )).data;
+      )).data
+
       const params = optionsToParameters({
         tableFileId: this.tableFile._id,
         treeFileId: this.treeFile._id,
@@ -108,31 +167,35 @@ export default {
       await pollUntilJobComplete(this.girderRest, this.job, job => this.job = job);
 
       if (this.job.status === 3) {
-        this.result = (await this.girderRest.get(`item/${modelFitSummaryItem._id}/download`)).data;
+        this.running = false;
+        this.result = csvParse((await this.girderRest.get(`item/${modelFitSummaryItem._id}/download`)).data);
+        this.resultColumns = this.result.columns.map(d => ({text: d, value: d, sortable: false}));
         this.plotUrl = `${this.girderRest.apiRoot}/item/${plotItem._id}/download`;
       }
-    },
-    selectTableFile() {
-      this.$refs.tableFile.click();
-    },
-    selectTreeFile() {
-      this.$refs.treeFile.click();
+      if (this.job.status === 4) {
+        this.running = false;
+      }
     },
     async uploadTable(file) {
-      this.tableFileName = file.name;
-      const uploader = new utils.Upload(this.girderRest, file, this.scratchFolder);
-      this.tableFile = await uploader.start();
-      const reader = new FileReader();
-      reader.addEventListener('loadend', e => {
-        const text = e.srcElement.result;
-        this.columns = csvParse(text).columns;
-      });
-      reader.readAsText(file);
+      if (file) {
+        this.tableFileName = file.name;
+        const uploader = new utils.Upload(this.girderRest, file, this.scratchFolder);
+        this.tableFile = await uploader.start();
+        const reader = new FileReader();
+        reader.addEventListener('loadend', e => {
+          const text = e.srcElement.result;
+          this.table = csvParse(text);
+          this.columns = csvParse(text).columns;
+        });
+        reader.readAsText(file);
+      }
     },
     async uploadTree(file) {
-      this.treeFileName = file.name;
-      const uploader = new utils.Upload(this.girderRest, file, this.scratchFolder);
-      this.treeFile = await uploader.start();
+      if (file) {
+        this.treeFileName = file.name;
+        const uploader = new utils.Upload(this.girderRest, file, this.scratchFolder);
+        this.treeFile = await uploader.start();
+      }
     },
   }
 }
---
+++ client/src/components/JsonDataTable.vue
@@ -0,0 +1,19 @@
+<template>
+<v-data-table
+  :headers="data.columns.map(d => ({text: d, value: d}))"
+  :items="data"
+  :hide-actions="hideActions"
+>
+  <template v-slot:items="props">
+    <td v-for="column in data.columns" v-bind:key="column">
+      {{ props.item[column] }}
+    </td>
+  </template>
+</v-data-table>
+</template>
+<script>
+export default {
+  name: 'json-data-table',
+  props: ['data', 'hide-actions'],
+};
+</script>
